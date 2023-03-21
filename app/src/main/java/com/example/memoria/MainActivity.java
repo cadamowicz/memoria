2823a19f134d096118c112c137984d702f99b9e7
cadamowicz <claudio@cwbtech.com.br>
2023-03-21 19:12:56 -0300

    Initial commit

---
+++ app/src/main/java/com/example/memoria/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.memoria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText texto;
+
+    Button salvar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        texto=findViewById(R.id.texto);
+        salvar=findViewById(R.id.salvar);
+
+        salvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent = new Intent(MainActivity.this, Main_Tela2.class);
+                intent.putExtra("memoria",texto.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+    }
+}
